957015150ce0c97ef7be9f9dd55c96cc29f0b818
“Esther <esthcarelle@email.com>
2023-05-29 16:26:25 +0200

    updated the jetpack compose navigation api version and added the navigation with a url argument

--- app/src/main/java/com/mine/myapplication/MainActivity.kt
+++ app/src/main/java/com/mine/myapplication/MainActivity.kt
@@ -1,22 +1,28 @@
 package com.mine.myapplication
 
+import android.annotation.SuppressLint
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavType
+import androidx.navigation.compose.*
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.mine.myapplication.ui.theme.UnsplashTheme
@@ -31,40 +37,116 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    GetImagesScreen(vm = PhotoViewModel())
+                    NavGraph()
                 }
             }
         }
     }
-}
 
-@Composable
-fun GetImagesScreen(vm: PhotoViewModel) {
-    LazyVerticalGrid(modifier = Modifier.fillMaxSize(), columns = GridCells.Fixed(2)) {
-        items(vm.imagesList) { photo ->
+    @Composable
+    fun NavGraph() {
+        val navController = rememberNavController()
+        NavHost(
+            navController = navController,
+            startDestination = "random"
+        ) {
+            /* creating route "home" */
+            composable(route = "random") {
+                /* Using composable function */
+                GetImagesScreen(PhotoViewModel(), onNavigateToDetailsScreen = { url ->
+                    navController.currentBackStackEntry?.arguments?.putString("url", url)
+                    navController.navigate(
+                        "details?url=${
+                            Uri.encode(url)
+                        }"
+                    )
+                })
+            }
+            composable(
+                route = "details?url={url}",
+                arguments = listOf(navArgument("url") { type = NavType.StringType })
+            ) {
+                /* Using composable function */
+                it.arguments?.getString("url")
+                    ?.let { url -> ShowImageDetails(url) }
+            }
+        }
+    }
+
+    @Composable
+    fun GetImagesScreen(vm: PhotoViewModel, onNavigateToDetailsScreen: (String) -> Unit) {
+        LazyVerticalGrid(modifier = Modifier.fillMaxSize(), columns = GridCells.Fixed(2)) {
+            items(vm.imagesList) { photo ->
+                Card(
+                    modifier = Modifier
+                        .height(170.dp)
+                        .clickable {
+                            photo.urls?.regular?.let { onNavigateToDetailsScreen(it) }
+                        }
+                        .padding(8.dp),
+                    shape = RoundedCornerShape(10.dp)
+                ) {
+                    AsyncImage(
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data(photo.urls?.regular)
+                            .build(),
+                        modifier = Modifier,
+                        contentDescription = "",
+                        contentScale = ContentScale.FillBounds
+                    )
+                }
+            }
+        }
+    }
+
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+    @Composable
+    fun ShowImageDetails(url: String, onBackClick: () -> Unit = {}) {
+        Scaffold(topBar = {
+            MyTopAppBar()
+        }, content = {
             Card(
                 modifier = Modifier
-                    .height(170.dp)
+                    .fillMaxSize()
                     .padding(8.dp),
                 shape = RoundedCornerShape(10.dp)
             ) {
                 AsyncImage(
                     model = ImageRequest.Builder(LocalContext.current)
-                        .data(photo.urls?.regular)
+                        .data(url)
                         .build(),
                     modifier = Modifier,
                     contentDescription = "",
                     contentScale = ContentScale.FillBounds
                 )
             }
-        }
+        })
+
     }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    UnsplashTheme {
+    @Composable
+    fun MyTopAppBar(onBackClick: () -> Unit = {}) {
+        TopAppBar(title = { Text(text = "Details") },
+            navigationIcon = {
+                IconButton(onClick = { onBackClick }) {
+                    Icon(Icons.Filled.ArrowBack, null)
+                }
+            },
+            actions = {
+                IconButton(onClick = { }) {
+                    Icon(Icons.Filled.Edit, null)
+                }
+            })
 
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        UnsplashTheme {
+            GetImagesScreen(vm = PhotoViewModel()) {
+
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/mine/myapplication/Navigation.kt
@@ -0,0 +1,8 @@
+package com.mine.myapplication
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PhotosNavigation(){
+
+}
