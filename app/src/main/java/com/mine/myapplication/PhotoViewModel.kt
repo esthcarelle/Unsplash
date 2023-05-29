95749b29964041316e2413ace04565bd4c75adf8
“Esther <esthcarelle@email.com>
2023-05-29 13:30:36 +0200

    Added an api key and retrieved images from the unsplash api

--- app/src/main/java/com/mine/myapplication/PhotoViewModel.kt
+++ app/src/main/java/com/mine/myapplication/PhotoViewModel.kt
@@ -1,7 +1,5 @@
 package com.mine.myapplication
 
-import android.content.ContentValues.TAG
-import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
@@ -11,28 +9,27 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 
 class PhotoViewModel : ViewModel() {
-    private val _todoList = mutableStateListOf<ResponseItem>()
-    var errorMessage: String by mutableStateOf("")
-    val todoList: List<ResponseItem>
-        get() = _todoList
+    private val _imagesList = mutableStateListOf<ResponseItem>()
+    private var errorMessage: String by mutableStateOf("")
+    val imagesList: List<ResponseItem>
+        get() = _imagesList
 
     init {
-        getTodoList()
+        getImages()
     }
 
-    private fun getTodoList() {
+    private fun getImages() {
         viewModelScope.launch {
             val apiService = APIService.getInstance()
             try {
-                _todoList.clear()
-                val list = apiService.getPhotos("Client-ID dixtqIxMkkn0gBKvye_yGfKHH3dUxemwT_QwBFwYW04", 20)
+                _imagesList.clear()
+                val list = apiService.getPhotos(
+                    "Client-ID dixtqIxMkkn0gBKvye_yGfKHH3dUxemwT_QwBFwYW04",
+                    20
+                )
 
                 list.forEach {
-                    if (it != null) {
-                        Log.e(TAG, "getTodoList: "+it )
-
-                        _todoList.add(it)
-                    }
+                    _imagesList.add(it)
                 }
 
             } catch (e: Exception) {
