aa5d9a156a76c63ed8046a856bc6141df321f3c4
“Esther <esthcarelle@email.com>
2023-05-31 00:41:44 +0200

    converted the url to bitmap

--- app/src/main/java/com/mine/myapplication/MainActivity.kt
+++ app/src/main/java/com/mine/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -24,21 +25,28 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Edit
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavType
 import androidx.navigation.compose.*
+import coil.ImageLoader
 import coil.compose.AsyncImage
+import coil.compose.rememberImagePainter
 import coil.request.ImageRequest
+import coil.request.SuccessResult
+import com.google.ar.core.Config
 import com.mine.myapplication.ui.theme.UnsplashTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -112,14 +120,24 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    suspend fun loadBitmapFromUrl(url: String, context:Context) : Bitmap{
+
+        val imageLoader: ImageLoader = ImageLoader(context = context)
+        val request: ImageRequest = ImageRequest.Builder(context = context).data(url).build()
+        val result = (imageLoader.execute(request = request) as SuccessResult).drawable
+
+        return (result as BitmapDrawable).bitmap
+
+    }
     @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     @Composable
     fun ShowImageDetails(url: String, onBackClick: () -> Unit = {}) {
-        val clickCount = remember { mutableStateOf(0f) }
-
+        val clickCount = remember { mutableStateOf(0.5f) }
+        val bitmapState = remember { mutableStateOf<Bitmap?>(null) }
+        val context = LocalContext.current
 
         Scaffold(topBar = {
-            MyTopAppBar(onBackClick = onBackClick, onEditClick = { clickCount.value = it})
+            MyTopAppBar(onBackClick = onBackClick, onEditClick = { clickCount.value = it })
         }, content = {
             Card(
                 modifier = Modifier
@@ -127,27 +145,32 @@ class MainActivity : ComponentActivity() {
                     .padding(8.dp),
                 shape = RoundedCornerShape(10.dp)
             ) {
-//                AsyncImage(
-//                    model = ImageRequest.Builder(LocalContext.current)
-//                        .data(url)
-//                        .build(),
-//                    modifier = Modifier.blur(radius = 50.dp),
-//                    contentDescription = "",
-//                    contentScale = ContentScale.FillBounds
-//                )
-                clickCount.value
-                val bitmap = BitmapFactory
-                    .decodeResource(LocalContext.current.resources, R.drawable.custom_image)
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(url)
+                        .build(),
+                    modifier = Modifier.blur(radius = 50.dp),
+                    contentDescription = "",
+                    contentScale = ContentScale.FillBounds
+                )
+                LaunchedEffect(url){
+                    bitmapState.value = loadBitmapFromUrl(url, context)
+                }
+
+//                val bitmap = BitmapFactory
+//                    .decodeResource(LocalContext.current.resources, R.drawable.custom_image)
 
                 if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
-                    LegacyBlurImage(bitmap, clickCount.value)
+                    bitmapState.value?.let { it1 -> LegacyBlurImage(it1, clickCount.value) }
                 } else {
-                    BlurImage(
-                        bitmap,
-                        Modifier
-                            .fillMaxSize()
-                            .blur(radiusX = clickCount.value.dp, radiusY = clickCount.value.dp)
-                    )
+                    bitmapState.value?.let { it1 ->
+                        BlurImage(
+                            it1,
+                            Modifier
+                                .fillMaxSize()
+                                .blur(radiusX = clickCount.value.dp, radiusY = clickCount.value.dp)
+                        )
+                    }
                 }
             }
 
@@ -163,7 +186,7 @@ class MainActivity : ComponentActivity() {
     ) {
 
         val renderScript = RenderScript.create(LocalContext.current)
-        val bitmapAlloc = Allocation.createFromBitmap(renderScript, bitmap)
+        val bitmapAlloc = Allocation.createFromBitmap(renderScript, bitmap.convertToSoftwareBitmap())
         ScriptIntrinsicBlur.create(renderScript, bitmapAlloc.element).apply {
             setRadius(blurRadio)
             setInput(bitmapAlloc)
@@ -174,7 +197,12 @@ class MainActivity : ComponentActivity() {
 
         BlurImage(bitmap, modifier)
     }
-
+    fun Bitmap.convertToSoftwareBitmap(): Bitmap {
+        if (config == Bitmap.Config.ARGB_8888) {
+            return this
+        }
+        return copy(Bitmap.Config.ARGB_8888, true)
+    }
     @Composable
     fun BlurImage(
         bitmap: Bitmap,
