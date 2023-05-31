ee591f9036690c3405e16d8f8297bfaa4c3fafdc
“Esther <esthcarelle@email.com>
2023-05-31 14:23:23 +0200

    extracted composables in their own classes and refactored the navigation graph

--- app/src/main/java/com/mine/myapplication/MainActivity.kt
+++ app/src/main/java/com/mine/myapplication/MainActivity.kt
@@ -1,45 +1,16 @@
 package com.mine.myapplication
 
 import android.annotation.SuppressLint
-import android.content.ContentValues.TAG
-import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
-import android.os.Build
 import android.os.Bundle
-import android.renderscript.Allocation
-import android.renderscript.RenderScript
-import android.renderscript.ScriptIntrinsicBlur
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Edit
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.blur
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.*
-import coil.ImageLoader
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
-import coil.request.SuccessResult
 import com.mine.myapplication.ui.theme.UnsplashTheme
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,16 +22,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
-                    PhotosNavGraph()
+                    HomeScreen()
                 }
             }
         }
     }
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     @Composable
     fun HomeScreen(){
-        Scaffold(bottomBar = ) {
-
+        val navController = rememberNavController()
+        Scaffold(bottomBar = { BottomNavigationGraph(navController = navController) }) {
+            BottomNavigationGraph(navController = navController)
         }
     }
 
