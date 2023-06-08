aa01f66a3f9c8e7b954c4f009aa6ba04d4bbf5a1
“Esther <esthcarelle@gmail.com>
2023-06-08 18:16:23 +0200

    Refactored the data class

--- app/src/main/java/com/mine/myapplication/model/PhotoEntity.kt
+++ app/src/main/java/com/mine/myapplication/model/PhotoEntity.kt
@@ -5,32 +5,21 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "images")
-class PhotoEntity {
+data class PhotoEntity(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "imageId")
-    var id: Int = 0
-
+    var id: Int = 0,
     @ColumnInfo(name = "url")
-    var url: String = ""
-
+    var url: String = "",
     @ColumnInfo(name = "imageState")
-    var imageState: String = ""
-
+    var imageState: String = "",
     @ColumnInfo(name = "offSetY")
-    var offSetY: Float = 0f
-
+    var offSetY: Float = 0f,
     @ColumnInfo(name = "offSetX")
-    var offSetX: Float = 0f
-
+    var offSetX: Float = 0f,
     @ColumnInfo(name = "scale")
-    var scale: Float = 1f
-
+    var scale: Float = 1f,
     @ColumnInfo(name = "rotation")
     var rotation: Float = 0f
+)
 
-    constructor() {}
-
-    constructor(url: String) {
-        this.url = url
-    }
-}
